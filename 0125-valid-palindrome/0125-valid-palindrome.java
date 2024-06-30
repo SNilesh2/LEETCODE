6fcf39d2a9d15e2e8545ff117ccf7cc0c1b85123
SNilesh2 <143276105+SNilesh2@users.noreply.github.com>
2024-06-30 15:51:57 +0530

    Time: 3 ms (73.12%), Space: 42.9 MB (63.91%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder normalized=new StringBuilder();
+        for(char c:s.toCharArray())
+        {
+            if(Character.isLetterOrDigit(c))
+            {
+                normalized.append(Character.toLowerCase(c));
+            }
+        }
+        int left=0;
+        int right=normalized.length()-1;
+        while(left<right)
+        {
+            if(normalized.charAt(left)!=normalized.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
