9275ee75866268b5adc94263a150b4e107e23a79
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-04-26 20:01:10 +0530

    Time: 25 ms (17.54%), Space: 144.1 MB (19.62%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean containsCycle(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] visited = new int[m][n];
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(visited[i][j]==0)
+                {
+                    if(dfs(grid[i][j],i,j,grid,visited,-1,-1,m,n))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean dfs(char initChar,int row,int col,char[][] grid,int[][] vis,int parRow,int parCol,int m,int n)
+    {
+        vis[row][col] = 1;
+
+        int[] drow = {0,1,0,-1};
+        int[] dcol = {1,0,-1,0};
+
+        for(int i=0;i<4;i++)
+        {
+            int newRow = row + drow[i]; 
+            int newCol = col + dcol[i];
+
+            if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && grid[newRow][newCol]==initChar && vis[newRow][newCol]==0)
+            {
+                if(dfs(initChar,newRow,newCol,grid,vis,row,col,m,n))
+                {
+                    return true;
+                }
+            }
+            else if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && grid[newRow][newCol]==initChar && vis[newRow][newCol]==1 && (newRow!=parRow || newCol!=parCol))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
