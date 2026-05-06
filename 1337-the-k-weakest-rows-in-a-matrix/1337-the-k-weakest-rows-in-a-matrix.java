0b6ff889731f30d24ea326324c3fa108651d5930
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-05-06 23:01:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -13,39 +13,29 @@ class Solution {
         int row = mat.length;
         int col = mat[0].length;
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->
-        {
-            if(a.maxOnes != b.maxOnes)
-                {
-                  return a.maxOnes - b.maxOnes;
-                }
-                else
-                {
-                  return a.ind - b.ind;
-                }
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
+            if (a.maxOnes != b.maxOnes) {
+                return a.maxOnes - b.maxOnes;
+            } else {
+                return a.ind - b.ind;
+            }
         });
 
-        for(int i=0;i<row;i++)
-        {
+        for (int i = 0; i < row; i++) {
             int noOfOnes = 0;
-            for(int j=0;j<col;j++)
-            {
-                if(mat[i][j]==1)
-                {
+            for (int j = 0; j < col; j++) {
+                if (mat[i][j] == 1) {
                     noOfOnes++;
-                }
-                else
-                {
+                } else {
                     break;
                 }
             }
-            pq.offer(new Pair(i,noOfOnes));
+            pq.offer(new Pair(i, noOfOnes));
         }
 
         int[] result = new int[k];
 
-        for(int i=0;i<k;i++)
-        {
+        for (int i = 0; i < k; i++) {
             result[i] = pq.poll().ind;
         }
 
