9c4a636dd0927eeb628d94e516b9a1b4b5a6a088
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-06-11 23:57:03 +0530

    Time: 0 ms (100%), Space: 42.2 MB (32.8%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,27 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1)
+        if(n==0)
         {
-            return n;
+            return 0;
         }
-        int last=fib(n-1);
-        int slast=fib(n-2);
-        return last+slast;
+
+        int[] memo = new int[n+1];
+        Arrays.fill(memo,-1);
+        return fibo(n,memo);
+    }
+
+    public static int fibo(int n,int[] memo)
+    {
+        if(memo[n]!=-1)
+        {
+            return memo[n];
+        }
+
+        if(n<=2)
+        {
+            return 1;
+        }
+
+        return memo[n] = fibo(n-1,memo) + fibo(n-2,memo);
     }
 }
