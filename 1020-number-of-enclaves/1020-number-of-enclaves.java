b3058628b924a65243b98e9f55ce40219d49af56
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-06-11 17:11:36 +0530

    Time: 9 ms (52.59%), Space: 66.3 MB (5.84%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,43 +1,76 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int[][] visited=new int[m][n];
-        for(int i=0;i<m;i++)
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int[][] vis = new int[rows][cols];
+
+        //first row
+        for(int col=0;col<cols;col++)
         {
-            for(int j=0;j<n;j++)
+            if(grid[0][col]==1)
             {
-                if((i==0 || j==0 || i==m-1 || j==n-1) && grid[i][j]==1)
+                dfs(0,col,grid,vis);
+            }
+        }
+
+        //last row
+        for(int col=0;col<cols;col++)
+        {
+            if(grid[rows-1][col]==1)
+            {
+                dfs(rows-1,col,grid,vis);
+            }
+        }
+
+        //first col
+        for(int row=1;row<rows-1;row++)
+        {
+            if(grid[row][0]==1)
+            {
+                dfs(row,0,grid,vis);
+            }
+        }
+
+        //last col
+        for(int row=1;row<rows-1;row++)
+        {
+            if(grid[row][cols-1]==1)
+            {
+                dfs(row,cols-1,grid,vis);
+            }
+        }
+
+        int result = 0;
+        for(int i=1;i<rows-1;i++)
+        {
+            for(int j=1;j<cols-1;j++)
+            {
+                if(grid[i][j]==1 && vis[i][j]==0)
                 {
-                    dfs(i,j,grid,visited,m,n);
+                    result++;
                 }
             }
         }
-        int cnt=0;
-        for(int i=1;i<m-1;i++)
-        {
-            for(int j=1;j<n-1;j++)
-            {
-                if(visited[i][j]==0 && grid[i][j]==1)
-                {
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
+
+        return result;
     }
-    int[] drow={-1,0,1,0};
-    int[] dcol={0,1,0,-1};
-    public void dfs(int row,int col,int[][] grid,int[][] visited,int m,int n)
+
+    public static void dfs(int row,int col,int[][] grid,int[][] vis)
     {
-        visited[row][col]=1;
+        vis[row][col] = 1;
+
+        int[] drow = {-1,0,1,0};
+        int[] dcol = {0,1,0,-1};
+
         for(int i=0;i<4;i++)
         {
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && visited[nrow][ncol]==0 && grid[nrow][ncol]==1)
+            int nr = row + drow[i];
+            int nc = col + dcol[i];
+
+            if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]==1 && vis[nr][nc]==0)
             {
-                dfs(nrow,ncol,grid,visited,m,n);
+                dfs(nr,nc,grid,vis);
             }
         }
     }
