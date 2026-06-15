e0342eebd0f641d62f224abebb07c843081f90c4
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-06-15 23:01:52 +0530

    Time: 0 ms (100%), Space: 42.1 MB (64.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,33 +1,30 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       /* int N=m+n-2;
-        int r=m-1;
-        double res=1;
-        for(int i=1;i<=r;i++)
+        int[][] memo = new int[m+1][n+1];
+        for(int[] arr : memo)
         {
-            res=res * (N-r+i)/i;
+            Arrays.fill(arr,-1);
         }
-        return (int)res;*/
-        int[][] dp=new int[m][n];
-        return fun(m,n,dp);
+        return helper(m,n,memo);
     }
-    public static int fun(int m,int n,int[][] dp)
+
+    public static int helper(int m,int n,int[][] memo)
     {
-        for(int i=0;i<m;i++)
-    {
-        for(int j=0;j<n;j++)
+        if(memo[m][n]!=-1)
         {
-            if(i==0 && j==0) dp[0][0]=1;
-            else
-            {
-                int up=0,left=0;
-                if(i>0) up=dp[i-1][j];
-                if(j>0) left=dp[i][j-1];
-                dp[i][j]=up+left;
-            }
+            return memo[m][n];
         }
+
+        if(m==1 && n==1)
+        {
+            return 1;
+        }
+
+        if(m==0 || n==0)
+        {
+            return 0;
+        }
+
+        return memo[m][n] = helper(m-1,n,memo) + helper(m,n-1,memo);
     }
-        return dp[m-1][n-1];
-    }
-   
 }
