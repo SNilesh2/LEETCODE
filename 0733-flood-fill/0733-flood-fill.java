4251e5f0d43534f96ed5329440fcf131d5024e10
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-08-28 07:28:16 +0530

    Time: 1 ms (39.26%), Space: 46.7 MB (79.26%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,56 +1,53 @@
 class Pair
 {
-    int row;
-    int col;
-    public Pair(int row,int col)
+    int r;
+    int c;
+    public Pair(int r,int c)
     {
-        this.row = row;
-        this.col = col;
+        this.r = r;
+        this.c = c;
     }
 }
-
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int row = image.length;
+        int col = image[0].length;
 
-        if(image[sr][sc]==color)
+        int original = image[sr][sc];
+
+        if(original==color)
         {
             return image;
         }
-        
-        int original = image[sr][sc];
-        int rowLen = image.length;
-        int colLen = image[0].length;
-
-        int[][] vis = new int[rowLen][colLen];
 
         Queue<Pair> q = new LinkedList<>();
+
         q.add(new Pair(sr,sc));
-        vis[sr][sc] = 1;
+        image[sr][sc] = color;
 
         int[] drow = {-1,0,1,0};
         int[] dcol = {0,1,0,-1};
 
         while(!q.isEmpty())
         {
-            Pair p = q.poll();
+            Pair polled = q.poll();
 
-            int row = p.row;
-            int col = p.col;
-
-            image[row][col] = color;
+            int r = polled.r;
+            int c = polled.c;
 
             for(int i=0;i<4;i++)
             {
-                int newRow = row + drow[i];
-                int newCol = col + dcol[i];
+                int nr = r + drow[i];
+                int nc = c + dcol[i];
 
-                if(newRow>=0 && newRow<rowLen && newCol>=0 && newCol<colLen && vis[newRow][newCol]==0 && image[newRow][newCol]==original)
+                if(nr>=0 && nr<row && nc>=0 && nc<col && image[nr][nc]==original)
                 {
-                    vis[newRow][newCol] = 1;
-                    q.add(new Pair(newRow,newCol));
+                    image[nr][nc] = color;
+                    q.add(new Pair(nr,nc));
                 }
             }
         }
+
         return image;
     }
 }
