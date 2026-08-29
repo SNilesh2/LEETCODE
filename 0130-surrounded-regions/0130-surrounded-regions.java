409e2d4cd1b33c753b5310f0bf0de3e6cf9cf8ad
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-08-29 11:42:07 +0530

    Time: 3 ms (28.55%), Space: 47.5 MB (93.05%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,46 +1,90 @@
+class Pair
+{
+    int r;
+    int c;
+    public Pair(int r,int c)
+    {
+        this.r = r;
+        this.c = c;
+    }
+}
+
 class Solution {
     public void solve(char[][] board) {
-        int rows = board.length;
-        int cols = board[0].length;
-        int[][] vis = new int[rows][cols];
+        int[][] vis = new int[board.length][board[0].length];
 
-        for(int i=0;i<rows;i++)
+        for(int i=0;i<board[0].length;i++)
         {
-            for(int j=0;j<cols;j++)
+            if(board[0][i]=='O')
             {
-                if((i==0 || j==0 || i==rows-1 || j==cols-1) && board[i][j]=='O')
-                {
-                    dfs(i,j,board,vis);
-                }
+                bfs(0,i,board,vis);
             }
         }
 
-        for(int i=1;i<rows;i++)
+        for(int i=0;i<board[0].length;i++)
         {
-            for(int j=1;j<cols;j++)
+            if(board[board.length-1][i]=='O')
             {
-                if(board[i][j]=='O' && vis[i][j]==0)
+                bfs(board.length-1,i,board,vis);
+            }
+        }
+
+        for(int i=0;i<board.length;i++)
+        {
+            if(vis[i][0]==0 && board[i][0]=='O')
+            {
+                bfs(i,0,board,vis);
+            }
+        }
+
+        for(int i=0;i<board.length;i++)
+        {
+            if(vis[i][board[0].length-1]==0 && board[i][board[0].length-1]=='O')
+            {
+                bfs(i,board[0].length-1,board,vis);
+            }
+        }
+
+
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[0].length;j++)
+            {
+                if(vis[i][j]==0 && board[i][j]=='O')
                 {
                     board[i][j] = 'X';
                 }
             }
         }
     }
-    public static void dfs(int row ,int col,char[][] board,int[][] vis)
+
+    public static void bfs(int r,int c,char[][] board,int[][] vis)
     {
-        vis[row][col] = 1;
+        Queue<Pair> q = new LinkedList<>();
+
+        q.add(new Pair(r,c));
+        vis[r][c] = 1;
 
         int[] drow = {-1,0,1,0};
         int[] dcol = {0,1,0,-1};
 
-        for(int i=0;i<4;i++)
+        while(!q.isEmpty())
         {
-            int nr = row + drow[i];
-            int nc = col + dcol[i];
+            Pair p = q.poll();
 
-            if(nr>=0 && nr<board.length && nc>=0 && nc<board[0].length && board[nr][nc]=='O' && vis[nr][nc]==0)
+            int row = p.r;
+            int col = p.c;
+
+            for(int i=0;i<4;i++)
             {
-                dfs(nr,nc,board,vis);
+                int nr = row + drow[i];
+                int nc = col + dcol[i];
+
+                if(nr>=0 && nr<board.length && nc>=0 && nc<board[0].length && vis[nr][nc]==0 && board[nr][nc]=='O')
+                {
+                    vis[nr][nc] = 1;
+                    q.add(new Pair(nr,nc));
+                }
             }
         }
     }
