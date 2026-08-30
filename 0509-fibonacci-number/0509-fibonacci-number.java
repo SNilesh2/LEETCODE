04d478f8145cb8948efc02118908edaa29cb3fb0
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-08-30 10:18:59 +0530

    Time: 0 ms (100%), Space: 42.1 MB (33.48%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,27 +1,51 @@
 class Solution {
     public int fib(int n) {
-        if(n==0)
+        // Recursion
+        // if(n<=1)
+        // {
+        //     return n;
+        // }
+        // return fib(n-1) + fib(n-2);
+
+        // Memoization
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp,-1);
+        // if(n<=1)
+        // {
+        //     return n;
+        // }
+        // if(dp[n]!=-1)
+        // {
+        //     return dp[n];
+        // }
+        // return dp[n] = fib(n-1) + fib(n-2);
+
+        // Tabulation
+        // int[] dp = new int[n+1];
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // for(int i=2;i<=n;i++)
+        // {
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+
+        // Space Optimization
+
+        if(n<=1)
         {
-            return 0;
+            return n;
+        }
+        int prev2 = 0;
+        int prev1 = 1;
+
+        for(int i=2;i<=n;i++)
+        {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        int[] memo = new int[n+1];
-        Arrays.fill(memo,-1);
-        return fibo(n,memo);
-    }
-
-    public static int fibo(int n,int[] memo)
-    {
-        if(memo[n]!=-1)
-        {
-            return memo[n];
-        }
-
-        if(n<=2)
-        {
-            return 1;
-        }
-
-        return memo[n] = fibo(n-1,memo) + fibo(n-2,memo);
+        return prev1;
     }
 }
