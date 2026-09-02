c2aca7a6fe1e48727372bf0862c665c8029992c7
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-09-02 09:26:53 +0530

    Time: 0 ms (100%), Space: 42.7 MB (56.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,26 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        findMax(nums,dp);
-        return dp[nums.length-1];
-    }
-    public static void findMax(int[] nums,int[] dp)
-    {
-        dp[0] = nums[0];
+        int prev2 = 0;
+        int prev1 = nums[0];
         for(int i=1;i<nums.length;i++)
         {
             int pick = nums[i];
             if(i-2 >= 0)
             {
-                pick = pick + dp[i-2];  
+                pick = pick + prev2;  
             }
             int notpick = Integer.MIN_VALUE;
             if(i-1 >= 0)
             {
-                notpick = 0 + dp[i-1];
+                notpick = 0 + prev1;
             }
 
-            dp[i] = Math.max(pick,notpick);
+            int curr = Math.max(pick,notpick);
+            prev2 = prev1;
+            prev1 = curr;
         }
+
+        return prev1;
     }
 }
