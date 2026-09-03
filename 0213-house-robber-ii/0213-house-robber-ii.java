a3f90f4abf82c9a231a8e973db1e9f1380a13e8c
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-09-03 08:04:11 +0530

    Time: 0 ms (100%), Space: 42.7 MB (78.02%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,28 +1,54 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1) return nums[0];
-        int[] temp1=new int[nums.length-1];
-        int[] temp2=new int[nums.length-1];
-        for(int i=0;i<nums.length;i++)
+        int size = nums.length;
+        int[] arr1 = new int[size-1];
+        int[] arr2 = new int[size-1];
+
+        int j=0,k=0;
+        for(int i=0;i<size;i++)
         {
-            if(i!=0) temp1[i-1]=nums[i];
-            if(i!=nums.length-1) temp2[i]=nums[i];
+            if(i!=0)
+            {
+                arr1[j++] = nums[i];
+            }
+            if(i!=size-1)
+            {
+                arr2[k++] = nums[i];
+            }
         }
-        return Math.max(solve(nums.length-1,temp1),solve(nums.length-1,temp2)); 
+
+        if(nums.length==1)
+        {
+            return nums[0];
+        }
+
+        return Math.max(findMax(arr1),findMax(arr2));
     }
-    public static int solve(int n,int[] nums)
+
+    public static int findMax(int[] nums)
     {
-        int prev1=nums[0];
-        int prev2=0;
-        for(int ind=1;ind<n;ind++)
+
+        int prev1 = nums[0];
+        int prev2 = 0;
+
+        for(int i=1;i<nums.length;i++)
         {
-            int pick=nums[ind];
-            if(ind>1) pick=pick+prev2;
-            int notpick=0+prev1;
-            int curri=Math.max(pick,notpick);
-            prev2=prev1;
-            prev1=curri;
+            int pick = nums[i];
+            if(i-2 >= 0)
+            {
+                pick = pick + prev2;
+            }
+            int notpick = 0;
+            if(i-1 >= 0)
+            {
+                notpick = prev1;
+            }
+
+            int curr = Math.max(pick,notpick);
+            prev2 = prev1;
+            prev1 = curr;
         }
+
         return prev1;
     }
 }
