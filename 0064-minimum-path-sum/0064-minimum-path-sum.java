56e3836b157e182ffeeae4196d374e6badfa047e
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-09-06 09:59:42 +0530

    Time: 4 ms (60.27%), Space: 50.3 MB (56.55%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,55 +3,38 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        int[][] dp = new int[m][n];
-
-        return findMinPath(m,n,grid,dp);
+        return findMinPath(m,n,grid);
     }
 
-    public int findMinPath(int r,int c,int[][] grid,int[][] dp)
+    public int findMinPath(int r,int c,int[][] grid)
     {
-        // if(r==0 && c==0)
-        // {
-        //     return grid[0][0];
-        // }
-        // if(r<0 || c<0)
-        // {
-        //     return Integer.MAX_VALUE;
-        // }
-        // if(dp[r][c]!=-1)
-        // {
-        //     return dp[r][c];
-        // }
-        // int top = findMinPath(r-1,c,grid,dp);
-        // int left = findMinPath(r,c-1,grid,dp);
-        // return dp[r][c] = Math.min(top,left) + grid[r][c];
+        int[] prev = new int[c];
+        Arrays.fill(prev,Integer.MAX_VALUE);
 
         for(int i=0;i<r;i++)
         {
+            int[] curr = new int[c];
             for(int j=0;j<c;j++)
             {
                 if(i==0 && j==0)
                 {
-                    dp[0][0] = grid[0][0];
+                    curr[0] = grid[0][0];
                     continue;
                 }
 
-                int top = Integer.MAX_VALUE;
-                if(i-1 >= 0)
-                {
-                    top = dp[i-1][j];
-                }
+                int top = prev[j];
 
                 int left = Integer.MAX_VALUE;
                 if(j-1 >= 0)
                 {
-                    left = dp[i][j-1];
+                    left = curr[j-1];
                 }
 
-                dp[i][j] = Math.min(top,left) + grid[i][j];
+                curr[j] = Math.min(top,left) + grid[i][j];
             }
+            prev = curr;
         }
 
-        return dp[r-1][c-1];
+        return prev[c-1];
     }
 }
