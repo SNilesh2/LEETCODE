f92cf0be0ce015ab10296cede621de84af33b859
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-09-06 09:52:26 +0530

    Time: 6 ms (9.95%), Space: 50.4 MB (21.54%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -5,35 +5,53 @@ class Solution {
 
         int[][] dp = new int[m][n];
 
-        for(int[] arr : dp)
-        {
-            Arrays.fill(arr,-1);
-        }
-
-        return findMinPath(m-1,n-1,grid,dp);
+        return findMinPath(m,n,grid,dp);
     }
 
     public int findMinPath(int r,int c,int[][] grid,int[][] dp)
     {
-        if(r==0 && c==0)
+        // if(r==0 && c==0)
+        // {
+        //     return grid[0][0];
+        // }
+        // if(r<0 || c<0)
+        // {
+        //     return Integer.MAX_VALUE;
+        // }
+        // if(dp[r][c]!=-1)
+        // {
+        //     return dp[r][c];
+        // }
+        // int top = findMinPath(r-1,c,grid,dp);
+        // int left = findMinPath(r,c-1,grid,dp);
+        // return dp[r][c] = Math.min(top,left) + grid[r][c];
+
+        for(int i=0;i<r;i++)
         {
-            return grid[0][0];
+            for(int j=0;j<c;j++)
+            {
+                if(i==0 && j==0)
+                {
+                    dp[0][0] = grid[0][0];
+                    continue;
+                }
+
+                int top = Integer.MAX_VALUE;
+                if(i-1 >= 0)
+                {
+                    top = dp[i-1][j];
+                }
+
+                int left = Integer.MAX_VALUE;
+                if(j-1 >= 0)
+                {
+                    left = dp[i][j-1];
+                }
+
+                dp[i][j] = Math.min(top,left) + grid[i][j];
+            }
         }
 
-        if(r<0 || c<0)
-        {
-            return Integer.MAX_VALUE;
-        }
-
-        if(dp[r][c]!=-1)
-        {
-            return dp[r][c];
-        }
-
-        int top = findMinPath(r-1,c,grid,dp);
-
-        int left = findMinPath(r,c-1,grid,dp);
-
-        return dp[r][c] = Math.min(top,left) + grid[r][c];
+        return dp[r-1][c-1];
     }
 }
