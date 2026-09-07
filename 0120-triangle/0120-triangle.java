61e995d619da5a43c17918833c722046e43c691e
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-09-07 09:35:37 +0530

    Time: 6 ms (20.87%), Space: 46.3 MB (26.71%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -23,13 +23,17 @@ class Solution {
         // int upLeft = findMinTotal(r-1,c-1,triangle,dp);
         // int upRight = findMinTotal(r-1,c,triangle,dp);
         // return dp[r][c] = triangle.get(r).get(c) + Math.min(upLeft,upRight);
+
+        int[] prev = new int[triangle.size()];
         for(int i=0;i<triangle.size();i++)
         {
+            int[] curr = new int[triangle.size()];
+
             for(int j=0;j<triangle.get(i).size();j++)
             {
                 if(i==0 && j==0)
                 {
-                    dp[0][0] = triangle.get(0).get(0);
+                    curr[0] = triangle.get(0).get(0);
                     continue;
                 }
 
@@ -37,25 +41,25 @@ class Solution {
 
                 if(j-1 >= 0)
                 {
-                    upLeft = dp[i-1][j-1];
+                    upLeft = prev[j-1];
                 }
 
                 int upRight = Integer.MAX_VALUE;
 
                 if(j<triangle.get(i-1).size())
                 {
-                    upRight = dp[i-1][j];
+                    upRight = prev[j];
                 }
 
-                dp[i][j] = triangle.get(i).get(j) + Math.min(upLeft,upRight);
+                curr[j] = triangle.get(i).get(j) + Math.min(upLeft,upRight);
             }
+            prev = curr;
         }
 
         int mini = Integer.MAX_VALUE;
-        int size = triangle.size();
-        for(int i=0;i<triangle.get(size-1).size();i++)
+        for(int i=0;i<triangle.size();i++)
         {
-            mini = Math.min(mini,dp[size-1][i]); 
+            mini = Math.min(mini,prev[i]); 
         }
 
         return mini;
