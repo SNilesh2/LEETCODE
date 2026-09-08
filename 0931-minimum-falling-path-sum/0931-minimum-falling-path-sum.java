3645d5e95478b76c7cf6392d6284455ff49e2dcb
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-09-08 08:25:08 +0530

    Time: 8 ms (26.11%), Space: 47.2 MB (46.67%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,27 +1,67 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int n=matrix.length;
-        int res=Integer.MAX_VALUE;
-        int[][] dp=new int[n][n];
-        for(int[] row:dp)
-        {
-            Arrays.fill(row,Integer.MAX_VALUE);
-        }
-        for(int j=0;j<n;j++)
-        {
-            int mini=fun(n-1,j,matrix,n,dp);
-            res=Math.min(res,mini);
-        }
-        return res;
+        int length = matrix.length;
+
+        int[][] dp = new int[length][length];
+
+        return findMinSum(matrix,dp);
     }
-    public static int fun(int i,int j,int[][] matrix,int n,int[][] dp)
+
+    public int findMinSum(int[][] matrix,int[][] dp)
     {
-        if(j<0 || j>n-1) return Integer.MAX_VALUE;
-        if(i==0) return matrix[i][j];
-        if(dp[i][j]!=Integer.MAX_VALUE) return dp[i][j];
-        int up=fun(i-1,j,matrix,n,dp);
-        int rightdiag=fun(i-1,j-1,matrix,n,dp);
-        int leftdiag=fun(i-1,j+1,matrix,n,dp);
-        return dp[i][j]=matrix[i][j]+Math.min(up,Math.min(rightdiag,leftdiag));
+        // if(c<0 || c>=matrix.length)
+        // {
+        //     return Integer.MAX_VALUE;
+        // }
+        // if(r==0)
+        // {
+        //     return matrix[0][c];
+        // }
+        // if(dp[r][c]!=-1)
+        // {
+        //     return dp[r][c];
+        // }
+        // int mini = Integer.MAX_VALUE;
+        // for(int i=-1;i<=1;i++)
+        // {
+        //     mini = Math.min(mini,findMinSum(r-1,c+i,matrix,dp));
+        // }
+        // return dp[r][c] = matrix[r][c] + mini;
+
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix.length;j++)
+            {
+                if(i==0)
+                {
+                    dp[0][j] = matrix[0][j];
+                    continue;
+                }
+
+                int upLeft = Integer.MAX_VALUE;
+                if(j-1 >= 0)
+                {
+                    upLeft = dp[i-1][j-1];
+                }
+
+                int up = dp[i-1][j];
+
+                int upRight = Integer.MAX_VALUE;
+                if(j+1 < matrix.length)
+                {
+                    upRight = dp[i-1][j+1];
+                }
+
+                dp[i][j] = matrix[i][j] + Math.min(upLeft,Math.min(up,upRight));
+            }
+        }
+
+        int mini = Integer.MAX_VALUE;
+        for(int i=0;i<matrix.length;i++)
+        {
+            mini = Math.min(mini,dp[matrix.length-1][i]);
+        }
+
+        return mini;
     }
 }
