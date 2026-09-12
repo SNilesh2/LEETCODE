e8a22ee737e94f86080a308b89e589b91f427453
NILESH S <143276105+SNilesh2@users.noreply.github.com>
2026-09-12 19:50:53 +0530

    Time: 41 ms (9.26%), Space: 47.4 MB (7.19%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,22 +1,38 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int left=0,right=0,maxf=0,maxlength=0;
-        int[] hash=new int[26];
-        while(right<s.length())
+        int left = 0;
+        int right = 0;
+        int maxLength = 0;
+        int maxf = 0;
+
+        HashMap<Character,Integer> map = new HashMap<>();
+        while(right < s.length())
         {
-            hash[s.charAt(right)-'A']++;
-            maxf=Math.max(maxf,hash[s.charAt(right)-'A']);
-            if(right-left+1-maxf>k)
+            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
+
+            maxf = Math.max(maxf,map.get(s.charAt(right)));
+
+            int length = right - left + 1;
+            if(length - maxf > k)
             {
-                hash[s.charAt(left)-'A']--;
+                map.put(s.charAt(left),map.get(s.charAt(left))-1);
                 left++;
+                int maxi = 0;
+                for(Map.Entry<Character,Integer> en : map.entrySet())
+                {
+                    maxi = Math.max(maxi,en.getValue());
+                }
+                maxf = maxi;
             }
-            if(right-left+1-maxf<=k)
+
+            if(length - maxf <= k)
             {
-                maxlength=Math.max(maxlength,right-left+1);
+                maxLength = Math.max(maxLength,length);
             }
+
             right++;
         }
-        return maxlength;
+
+        return maxLength;
     }
 }
